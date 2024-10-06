841e92ba9c67588de67db6777369cda7ee10f8b0
Ahmed Maher <ahmedsayed_g@sci.asu.edu.eg>
2024-10-06 20:09:28 +0300

    Add Generic Repository and Unit Of Work

---
+++ ECommerce.DataAccess/Implementation/UnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿using ECommerce.DataAccess.Data;
+using ECommerce.Entities.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.DataAccess.Implementation
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> CompleteAsync()
+            => await _context.SaveChangesAsync();
+
+        public async ValueTask DisposeAsync()
+            =>await _context.DisposeAsync();
+    }
+}
