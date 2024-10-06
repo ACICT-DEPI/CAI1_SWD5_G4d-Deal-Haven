db4f6c6aa71d99355be8cfddbc8ae33d634cf849
Ahmed Maher <ahmedsayed_g@sci.asu.edu.eg>
2024-10-07 00:08:25 +0300

    Add ProductRepo, ShoppingCartRepo

---
+++ ECommerce.DataAccess/Implementation/ProductRepository.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using ECommerce.DataAccess.Data;
+using ECommerce.Entities.Models;
+using ECommerce.Entities.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.DataAccess.Implementation
+{
+    public class ProductRepository : GenericRepository<Product>, IProductRepository
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public ProductRepository(ApplicationDbContext context, IMapper mapper) : base(context)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task Update(Product product)
+        {
+           var productFromDB = await _context.Products.FirstOrDefaultAsync(p => p.Id == product.Id);
+
+            if(productFromDB is not null)
+                productFromDB = _mapper.Map<Product>(product);
+        }
+    }
+}
